bcecc0eead158991a3555e6acbe798fff43b94d6
Romain Crocq-Marchand <romain.crocq-marchand@epitech.eu>
2025-09-01 16:22:34 +0200

    feat: :sparkles: complete rest infrastructure of timetable

--- backend/src/routes/timetables.routes.ts
+++ backend/src/routes/timetables.routes.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import { json, Router } from "express";
+import { Router } from "express";
 import { authenticateToken } from "../middleware/auth";
 
 const router = Router();
@@ -16,6 +16,30 @@ router.get("/", authenticateToken, async (req, res) => {
       },
     });
     res.json(timetables);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+router.get("/:id", authenticateToken, async (req, res) => {
+  try {
+    if (!req.user) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    const timetable = await prisma.savedTimetable.findUnique({
+      where: {
+        id: req.params.id,
+        userId: req.user.id,
+      },
+    });
+
+    if (!timetable) {
+      return res.status(404).json({ error: "Not found" });
+    }
+
+    res.json(timetable);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal server error" });
@@ -39,6 +63,51 @@ router.post("/", authenticateToken, async (req, res) => {
     });
 
     res.status(201).json(newTimetable);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+router.put("/:id", authenticateToken, async (req, res) => {
+  try {
+    if (!req.user) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    const { timetable } = req.body;
+
+    const updatedTimetable = await prisma.savedTimetable.update({
+      where: {
+        id: req.params.id,
+        userId: req.user.id,
+      },
+      data: {
+        timetable,
+      },
+    });
+
+    res.json(updatedTimetable);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+router.delete("/:id", authenticateToken, async (req, res) => {
+  try {
+    if (!req.user) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    const deletedTimetable = await prisma.savedTimetable.delete({
+      where: {
+        id: req.params.id,
+        userId: req.user.id,
+      },
+    });
+
+    res.json(deletedTimetable);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal server error" });
