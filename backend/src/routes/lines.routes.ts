7dbff44227e6ba9241bc0983d39b3d6704388a70
Romain Crocq-Marchand <romain.crocq-marchand@epitech.eu>
2025-09-12 16:23:12 +0200

    feat: :sparkles: use of axios to get metro line

--- backend/src/routes/lines.routes.ts
+++ backend/src/routes/lines.routes.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { Router } from "express";
 
 const router = Router();
@@ -5,9 +6,18 @@ const router = Router();
 router.get("/", async (req, res) => {
   try {
     // Fetch lines logic here
-    res.json({ id: "1001", name: "a", color: "blue" });
+    axios.get("https://data.explore.star.fr/api/explore/v2.1/catalog/datasets/tco-metro-topologie-lignes-td/records?select=id%2Cnomcourt%2Ccouleurligne&order_by=id&limit=20").then((response) => {
+      let lines = response.data.results.map((line: any) => ({
+        id: line.id,
+        name: line.nomcourt,
+        color: line.couleurligne,
+      }));
+      res.json(lines);
+    }).catch((error) => {
+      console.error("Error fetching lines from external API:", error);
+      throw error;
+    });
   } catch (error) {
-    console.error(error);
     res.status(500).json({ error: "Internal server error" });
   }
 });
