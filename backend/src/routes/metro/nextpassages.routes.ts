a3f4f4d6b2b15c60554149dc1d7412055d527818
Romain Crocq-Marchand <romain.crocq-marchand@epitech.eu>
2025-09-12 17:06:56 +0200

    feat: :sparkles: add new route for metro

--- backend/src/routes/metro/nextpassages.routes.ts
+++ backend/src/routes/metro/nextpassages.routes.ts
@@ -1,18 +1,24 @@
+import axios from "axios";
 import { Router } from "express";
 
 const router = Router();
 
-router.get("/", async (req, res) => {
+router.get("/:idStop", async (req, res) => {
   try {
-    // Fetch next passages logic here
-    res.json({
-      id: "1001-1-5431",
-      name: "Pontchaillou",
-      direction: "1",
-      idLine: "1001",
-      nextTrain: "10:30",
-      coordonnes: { lat: 48.123456, lon: -1.234567 },
-      extractionTime: "2023-10-10T10:00:00Z",
+    axios.get(`https://data.explore.star.fr/api/explore/v2.1/catalog/datasets/tco-metro-circulation-deux-prochains-passages-tr/records?limit=20&select=idjdd%2Cidligne%2Cnomarret%2Csens%2Carriveefirsttrain%2Ccoordonnees%2Cheureextraction&refine=idjdd%3A${req.params.idStop}`).then((response) => {
+      let passages = response.data.results.map((passage: any) => ({
+        id: passage.idjdd,
+        lineId: passage.idligne,
+        name: passage.nomarret,
+        direction: passage.sens,
+        nextTrain: passage.arriveefirsttrain,
+        coordonnees: passage.coordonnees,
+        extraction: passage.heureextraction,
+      }));
+      res.json(passages);
+    }).catch((error) => {
+      console.error("Error fetching next passages from external API:", error);
+      throw error;
     });
   } catch (error) {
     console.error(error);
--- backend/src/routes/metro/stops.routes.ts
+++ backend/src/routes/metro/stops.routes.ts
@@ -1,15 +1,21 @@
+import axios from "axios";
 import { Router } from "express";
 
 const router = Router();
 
-router.get("/", async (req, res) => {
+router.get("/:idLigne", async (req, res) => {
   try {
     // Fetch stops logic here
-    res.json({
-      id: "1001-1-5431",
-      name: "Pontchaillou",
-      direction: "1",
-      order: 1,
+    axios.get(`https://data.explore.star.fr/api/explore/v2.1/catalog/datasets/tco-metro-circulation-deux-prochains-passages-tr/records?select=nomarret%2Cidjdd%2Csens&limit=50&refine=idligne%3A%22${req.params.idLigne}%22`).then((response) => {
+      let stops = response.data.results.map((stop: any) => ({
+        id: stop.idjdd,
+        name: stop.nomarret,
+        direction: stop.sens,
+      }));
+      res.json(stops);
+    }).catch((error) => {
+      console.error("Error fetching stops from external API:", error);
+      throw error;
     });
   } catch (error) {
     console.error(error);
