632eca63bf6a92e2f9e1ce0d8fa328e065828792
Romain Crocq-Marchand <romain.crocq-marchand@epitech.eu>
2025-09-18 10:53:09 +0200

    feat: :sparkles: get token from the mobile
    no connected to the database yet

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,8 @@
 import dotenv from "dotenv";
 import express from "express";
-import metroRouter from "./routes/metro/mainRouter";
 import busRouter from "./routes/bus/mainRouter";
+import { sendNotification } from "./routes/firebase.routes";
+import metroRouter from "./routes/metro/mainRouter";
 import timetablesRoutes from "./routes/timetables.routes";
 import userRoutes from "./routes/users.routes";
 
@@ -17,6 +18,21 @@ app.use("/api/timetables", timetablesRoutes);
 // Metro Route
 app.use("/api/metro", metroRouter);
 app.use("/api/bus", busRouter);
+
+app.get("/test", (_req, res) => {
+  sendNotification(
+    "fcm_token_here",
+    "Test Notification",
+    "This is a test notification from Transit Track'er"
+  );
+  res.send("Notification sent");
+});
+
+app.post("/register-token", (req, res) => {
+  const token = req.body.token;
+  console.log("Received FCM token:", token);
+  res.status(200).send("Token registered");
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
