6ea09ab7c74ac9655964289cd2181e53fdc2cd48
Romain Crocq-Marchand <romain.crocq-marchand@epitech.eu>
2025-09-19 15:17:15 +0200

    refactor: :bricks: unificate of route nextpassage for bus
    It was too different compared to metro, the change simplify the worker

--- backend/src/routes/bus/nextpassages.routes.ts
+++ backend/src/routes/bus/nextpassages.routes.ts
@@ -3,16 +3,16 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get("/:idLigne/:idStop", async (req, res) => {
+router.get("/:id", async (req, res) => {
   try {
-    // Fetch next passages logic here
-    axios.get(`https://data.explore.star.fr/api/explore/v2.1/catalog/datasets/tco-bus-circulation-passages-tr/records?limit=20&refine=idarret%3A${req.params.idStop}&refine=idligne%3A%22${req.params.idLigne}%22`).then((response) => {
+    const [idLigne, idStop] = req.params.id.split("-");
+    axios.get(`https://data.explore.star.fr/api/explore/v2.1/catalog/datasets/tco-bus-circulation-passages-tr/records?limit=20&refine=idarret%3A${idStop}&refine=idligne%3A%22${idLigne}%22`).then((response) => {
       let passages = response.data.results.map((passage: any) => ({
-        id: passage.idjdd,
+        id: req.params.id,
         lineId: passage.idligne,
         name: passage.nomarret,
         direction: passage.sens,
-        nextTrain: passage.arriveefirsttrain,
+        nextTrain: passage.arrivee,
         coordonnees: passage.coordonnees,
         extraction: passage.heureextraction,
       }));
